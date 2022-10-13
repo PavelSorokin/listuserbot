bb8d1646af481996f02f9a0a12b775a2ade5a979
pashamoss <35258062+PavelSorokin@users.noreply.github.com>
2022-10-13 18:04:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,3 @@
 from telebot.storage import StateMemoryStorage
-
-main_bot_token = '5320419353:AAEPbL94lz3PG7blljjwqT4K0f0FRCyabdI'
 timeout_seconds = 60
-admins = [394011279,396471113,420553555] 
-#394011279,396471113,
-zerkalo_chat_id = '-1001687542001' #-
 state_storage = StateMemoryStorage()
