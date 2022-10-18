eef54bbfc771e35a4c46d741f50ec3aee8bb4a7d
pashamoss <pavel-sorokin69@list.ru>
2022-10-18 18:19:01 +0300

    zz

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
 from telebot.storage import StateMemoryStorage
 timeout_seconds = 60
-<<<<<<< HEAD
 admins = [396471113,420553555] 
 stas = 420553555
 #394011279,396471113,
 zerkalo_chat_id = '-1001687542001' #-
 state_storage = StateMemoryStorage()
-=======
-state_storage = StateMemoryStorage()
->>>>>>> bb8d1646af481996f02f9a0a12b775a2ade5a979
